258e28262636aa55b138cfd13ae0723d62fdb5b8
GeorgeTheJournalist <24482750+georgethejournalist@users.noreply.github.com>
2020-09-03 19:28:21 +0200

    Added script for slider that auto-updates it's Text field

---
+++ Assets/Scripts/UI/SliderWithValue.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderWithValue : MonoBehaviour
+{
+
+    public Slider Slider;
+    public Text Text;
+    public string Unit;
+    public byte Decimals = 2;
+
+    void OnEnable()
+    {
+        Slider.onValueChanged.AddListener(ChangeValue);
+        ChangeValue(Slider.value);
+    }
+    void OnDisable()
+    {
+        Slider.onValueChanged.RemoveAllListeners();
+    }
+
+    void ChangeValue(float value)
+    {
+        Text.text = value.ToString("n" + Decimals) + " " + Unit;
+    }
+
+
+}
