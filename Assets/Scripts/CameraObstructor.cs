b18039d05c6878fa2db1d575cfafa703a0f7734a
GeorgeTheJournalist <24482750+georgethejournalist@users.noreply.github.com>
2020-09-03 19:30:19 +0200

    Added script for positioning stuff in front of camera

---
+++ Assets/Scripts/CameraObstructor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraObstructor : MonoBehaviour
+{
+    public Camera CameraToAnnoy;
+    public float Distance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = CameraToAnnoy.transform.position + CameraToAnnoy.transform.forward * Distance;
+        transform.rotation = CameraToAnnoy.gameObject.transform.rotation;
+    }
+}
