14dcf390f6f2960cee5e3a6e139a355d3c491d77
GeorgeTheJournalist <24482750+georgethejournalist@users.noreply.github.com>
2020-09-03 19:40:34 +0200

    Updated namespaces in utility scripts

--- Assets/Scripts/Utils/TerrainUtils.cs
+++ Assets/Scripts/Utils/TerrainUtils.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace Assets.Scripts
+namespace Assets.Scripts.Utils
 {
     public static class TerrainUtils
     {
