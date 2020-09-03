edfa0323b742d77ade9a7764ecc3a4c04c35dbc6
GeorgeTheJournalist <24482750+georgethejournalist@users.noreply.github.com>
2020-09-03 19:41:08 +0200

    Added event args to their own folder

---
+++ Assets/Scripts/EventArgs/BurntTreesEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurntTreesEventArgs : EventArgs
+{
+    public List<Vector2> BurntTrees;
+
+    public BurntTreesEventArgs()
+    {
+        BurntTrees = new List<Vector2>();
+    }
+}
